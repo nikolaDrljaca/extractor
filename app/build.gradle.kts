3fe5f49aacf386bea734cbde3b63661e63f8fba0
nikolaDrljaca <devnikoladr@gmail.com>
2023-08-10 15:06:38 +0200

    gradle setup and deps. FINALLY building

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -31,17 +32,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
         resources {
@@ -55,7 +56,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -71,12 +72,36 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
+    //Coil
     implementation("io.coil-kt:coil-compose:2.4.0")
 
+    //ML Kit
     implementation("com.google.mlkit:image-labeling:17.0.7")
     implementation("com.google.mlkit:text-recognition:16.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
 
-    implementation("com.bumble.appyx:appyx-navigation:2.0.0-alpha01")
-    implementation("com.bumble.appyx:backstack-android:2.0.0-alpha01")
+    //Appyx
+    val appyxVersion = "2.0.0-alpha01"
+    implementation("com.bumble.appyx:appyx-navigation:$appyxVersion")
+    implementation("com.bumble.appyx:backstack-android:$appyxVersion")
+    implementation("com.bumble.appyx:spotlight-android:$appyxVersion")
+
+    //Koin
+    val koinAndroidComposeVersion = "3.4.6"
+    val koinAndroidVersion = "3.4.3"
+    implementation("io.insert-koin:koin-android:$koinAndroidVersion")
+    implementation("io.insert-koin:koin-androidx-workmanager:$koinAndroidVersion")
+    implementation("io.insert-koin:koin-androidx-compose:$koinAndroidComposeVersion")
+
+    //Room
+    val roomVersion = "2.5.2"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+    //WorkManager
+    val workVersion = "2.8.1"
+    implementation("androidx.work:work-runtime-ktx:$workVersion")
 }
