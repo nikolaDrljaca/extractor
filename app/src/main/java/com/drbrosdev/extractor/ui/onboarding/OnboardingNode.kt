84da24dd0c64631fb9da53c94a809a451ada8f3e
nikolaDrljaca <devnikoladr@gmail.com>
2023-08-29 14:18:39 +0200

    feat: application icon as imageBitmap

--- app/src/main/java/com/drbrosdev/extractor/ui/onboarding/OnboardingNode.kt
+++ app/src/main/java/com/drbrosdev/extractor/ui/onboarding/OnboardingNode.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,7 +18,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material.Text
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -45,7 +45,7 @@ import com.drbrosdev.extractor.R
 import com.drbrosdev.extractor.ui.onboarding.worker.StartWorkerNode
 import com.drbrosdev.extractor.ui.theme.md_theme_light_secondary
 import com.drbrosdev.extractor.ui.theme.md_theme_light_tertiary
-import com.drbrosdev.extractor.util.adaptiveIconPainterResource
+import com.drbrosdev.extractor.util.applicationIconBitmap
 import kotlinx.parcelize.Parcelize
 
 sealed interface OnboardingRoutes : Parcelable {
@@ -131,10 +131,7 @@ class OnboardingNode(
                     )
                     .fillMaxWidth(),
             ) {
-                Icon(
-                    painter = adaptiveIconPainterResource(id = R.mipmap.ic_launcher),
-                    contentDescription = "App Icon"
-                )
+                Image(bitmap = applicationIconBitmap(), contentDescription = "")
                 Spacer(modifier = Modifier.height(24.dp))
                 Text(
                     text = stringResource(id = R.string.app_name),
