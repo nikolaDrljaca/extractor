6d09286040c5e554e53fbc1dc4e13ece8bba5cbb
nikolaDrljaca <devnikoladr@gmail.com>
2023-08-29 13:59:12 +0200

    fix: move buttons to separate file

---
+++ app/src/main/java/com/drbrosdev/extractor/ui/components/Buttons.kt
@@ -0,0 +1,89 @@
+package com.drbrosdev.extractor.ui.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.drbrosdev.extractor.ui.theme.md_theme_light_primary
+
+@Composable
+fun ExtractorActionButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable (RowScope.() -> Unit)
+) {
+    val verticalContentPadding = 24.dp
+    val leftContentPadding = ButtonDefaults.ContentPadding.calculateLeftPadding(
+        LayoutDirection.Ltr
+    )
+    val rightContentPadding = ButtonDefaults.ContentPadding.calculateRightPadding(
+        LayoutDirection.Ltr
+    )
+
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .then(modifier),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = md_theme_light_primary
+        ),
+        contentPadding = PaddingValues(
+            top = verticalContentPadding,
+            bottom = verticalContentPadding,
+            start = leftContentPadding,
+            end = rightContentPadding
+        ),
+        shape = RoundedCornerShape(18.dp)
+    ) {
+        content()
+    }
+}
+
+
+@Composable
+fun OutlinedExtractorActionButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable (RowScope.() -> Unit)
+) {
+    val verticalContentPadding = 24.dp
+    val leftContentPadding = ButtonDefaults.ContentPadding.calculateLeftPadding(
+        LayoutDirection.Ltr
+    )
+    val rightContentPadding = ButtonDefaults.ContentPadding.calculateRightPadding(
+        LayoutDirection.Ltr
+    )
+
+    OutlinedButton(
+        onClick = onClick,
+        modifier = Modifier
+            .then(modifier),
+        colors = ButtonDefaults.outlinedButtonColors(
+            contentColor = Color.White
+        ),
+        contentPadding = PaddingValues(
+            top = verticalContentPadding,
+            bottom = verticalContentPadding,
+            start = leftContentPadding,
+            end = rightContentPadding
+        ),
+        shape = RoundedCornerShape(18.dp),
+        border = ButtonDefaults.outlinedButtonBorder.copy(
+            width = 1.dp,
+            brush = Brush.linearGradient(
+                listOf(Color.White, Color.White)
+            )
+        )
+    ) {
+        content()
+    }
+}
--- app/src/main/java/com/drbrosdev/extractor/ui/components/OnboardingCard.kt
+++ app/src/main/java/com/drbrosdev/extractor/ui/components/OnboardingCard.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,14 +14,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,7 +28,6 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.drbrosdev.extractor.R
 import com.drbrosdev.extractor.ui.theme.ExtractorTheme
@@ -114,79 +108,6 @@ fun OnboardingCardHeadline(
     }
 }
 
-@Composable
-fun ExtractorActionButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    content: @Composable (RowScope.() -> Unit)
-) {
-    val verticalContentPadding = 24.dp
-    val leftContentPadding = ButtonDefaults.ContentPadding.calculateLeftPadding(
-        LayoutDirection.Ltr
-    )
-    val rightContentPadding = ButtonDefaults.ContentPadding.calculateRightPadding(
-        LayoutDirection.Ltr
-    )
-
-    Button(
-        onClick = onClick,
-        modifier = Modifier
-            .then(modifier),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.White,
-            contentColor = md_theme_light_primary
-        ),
-        contentPadding = PaddingValues(
-            top = verticalContentPadding,
-            bottom = verticalContentPadding,
-            start = leftContentPadding,
-            end = rightContentPadding
-        ),
-        shape = RoundedCornerShape(18.dp)
-    ) {
-        content()
-    }
-}
-
-
-@Composable
-fun OutlinedExtractorActionButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    content: @Composable (RowScope.() -> Unit)
-) {
-    val verticalContentPadding = 24.dp
-    val leftContentPadding = ButtonDefaults.ContentPadding.calculateLeftPadding(
-        LayoutDirection.Ltr
-    )
-    val rightContentPadding = ButtonDefaults.ContentPadding.calculateRightPadding(
-        LayoutDirection.Ltr
-    )
-
-    OutlinedButton(
-        onClick = onClick,
-        modifier = Modifier
-            .then(modifier),
-        colors = ButtonDefaults.outlinedButtonColors(
-            contentColor = Color.White
-        ),
-        contentPadding = PaddingValues(
-            top = verticalContentPadding,
-            bottom = verticalContentPadding,
-            start = leftContentPadding,
-            end = rightContentPadding
-        ),
-        shape = RoundedCornerShape(18.dp),
-        border = ButtonDefaults.outlinedButtonBorder.copy(
-            width = 1.dp,
-            brush = Brush.linearGradient(
-                listOf(Color.White, Color.White)
-            )
-        )
-    ) {
-        content()
-    }
-}
 
 @Preview(uiMode = UI_MODE_NIGHT_NO)
 @Composable
