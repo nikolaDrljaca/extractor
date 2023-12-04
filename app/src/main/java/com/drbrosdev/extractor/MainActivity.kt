9e2f8afe36662f596b3f6670b19765eedeed37e5
nikolaDrljaca <devnikoladr@gmail.com>
2023-12-04 21:20:32 +0100

    cleanup

--- app/src/main/java/com/drbrosdev/extractor/MainActivity.kt
+++ app/src/main/java/com/drbrosdev/extractor/MainActivity.kt
@@ -1,29 +1,13 @@
 package com.drbrosdev.extractor
 
-import android.app.Activity
-import android.content.Context
-import android.content.ContextWrapper
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import com.drbrosdev.extractor.ui.root.Root
 import com.drbrosdev.extractor.ui.theme.ExtractorTheme
 
@@ -35,8 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExtractorTheme(dynamicColor = false) {
                 Surface(
-                    modifier = Modifier
-                        .fillMaxSize(),
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Root()
@@ -44,57 +27,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Composable
-fun RequestPermissionScreen(
-    onRequestPermission: () -> Unit,
-    onOpenAppSettings: () -> Unit
-) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(
-            12.dp,
-            alignment = Alignment.CenterVertically
-        ),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = {
-            onRequestPermission()
-        }) {
-            Text(text = "Request Image permission")
-        }
-
-        Button(onClick = onOpenAppSettings) {
-            Text(text = "Open App Settings")
-        }
-    }
-}
-
-//TODO Move this under ActivityExt file
-fun Activity.openAppSettings() {
-    Intent(
-        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-        Uri.fromParts("package", packageName, null)
-    ).also { startActivity(it) }
-}
-
-//TODO Move this to ContextExt file, under utils or extensions
-fun Context.checkAndRequestPermission(
-    permission: String,
-    launcher: ManagedActivityResultLauncher<String, Boolean>
-) {
-    val result = ContextCompat.checkSelfPermission(this, permission)
-    if (result != PackageManager.PERMISSION_GRANTED) {
-        launcher.launch(permission)
-    }
-}
-
-fun Context.findActivity(): Activity {
-    var context = this
-    while (context is ContextWrapper) {
-        if (context is Activity) return context
-        context = context.baseContext
-    }
-    error("No activity found.")
 }
