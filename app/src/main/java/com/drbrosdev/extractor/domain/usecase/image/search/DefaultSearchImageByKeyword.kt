937b52de992c7cb5d2d93290da57a946a96f5a9c
nikolaDrljaca <devnikoladr@gmail.com>
2024-02-05 20:40:51 +0100

    refactor: rework search query in dao

--- app/src/main/java/com/drbrosdev/extractor/domain/usecase/image/search/DefaultSearchImageByKeyword.kt
+++ app/src/main/java/com/drbrosdev/extractor/domain/usecase/image/search/DefaultSearchImageByKeyword.kt
@@ -5,57 +5,58 @@ import com.drbrosdev.extractor.data.dao.ImageEmbeddingsDao
 import com.drbrosdev.extractor.domain.model.DateRange
 import com.drbrosdev.extractor.domain.model.Extraction
 import com.drbrosdev.extractor.domain.model.KeywordType
-import com.drbrosdev.extractor.domain.model.SearchType
 import com.drbrosdev.extractor.domain.model.isIn
-import com.drbrosdev.extractor.domain.repository.payload.ImageEmbeddingSearchStrategy
+import com.drbrosdev.extractor.domain.usecase.TokenizeText
 import com.drbrosdev.extractor.util.logInfo
 import com.drbrosdev.extractor.util.toExtraction
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.withContext
 
 class DefaultSearchImageByKeyword(
     private val dispatcher: CoroutineDispatcher,
     private val imageEmbedDao: ImageEmbeddingsDao,
+    private val tokenizeText: TokenizeText
 ) : SearchImageByKeyword {
 
     override suspend fun execute(params: SearchImageByKeyword.Params): List<Extraction> =
         withContext(dispatcher) {
-            val result = with(params) {
-                query
-                    .prepareQuery()
-                    .mapToSearchStrategy(type)
-                    .findBy(keywordType)
-                    .filterByDateRange(dateRange)
+            val cleanQuery = tokenizeText(params.query)
+                .map { it.text }
+                .toList()
+
+            logInfo("Clean query: $cleanQuery")
+
+            val visualAdaptedQuery = buildString {
+                append("%")
+                append(
+                    cleanQuery
+                        .sorted()
+                        .joinToString(separator = "%") { it }
+                )
+                append("%")
             }
 
+            val ftsAdaptedQuery = buildString {
+                append(cleanQuery.joinToString(separator = " ") { it })
+                append("*")
+            }
+
+            logInfo("Visual Query: $visualAdaptedQuery | FTS query: $ftsAdaptedQuery")
+
+            val result = when (params.keywordType) {
+                KeywordType.ALL -> imageEmbedDao.findByKeyword(visualAdaptedQuery, ftsAdaptedQuery)
+                KeywordType.TEXT -> imageEmbedDao.findByTextEmbeddingFts(ftsAdaptedQuery)
+                KeywordType.IMAGE -> imageEmbedDao.findByVisualEmbedding(visualAdaptedQuery)
+            }
+
+            logInfo("Result set size: ${result.size}")
+
             result
+                .parMap(context = dispatcher) { it.imageEntity.toExtraction() }
+                .filterByDateRange(params.dateRange)
         }
-
-    private fun String.prepareQuery(): String {
-        return this
-            .trim()
-            .replace(Regex("\\s+"), " ")
-            .lowercase()
-    }
-
-    private fun String.mapToSearchStrategy(type: SearchType): ImageEmbeddingSearchStrategy {
-        return when (type) {
-            SearchType.FULL -> ImageEmbeddingSearchStrategy.Full(this)
-            SearchType.PARTIAL -> ImageEmbeddingSearchStrategy.Partial(this)
-        }
-    }
-
-    private suspend fun ImageEmbeddingSearchStrategy.findBy(keywordType: KeywordType): List<Extraction> {
-        logInfo("Search query/keyword: $query")
-        val imageEmbeddingRelations = when (keywordType) {
-            KeywordType.ALL -> imageEmbedDao.findByKeyword(query)
-            KeywordType.TEXT -> imageEmbedDao.findByTextEmbeddingFts(query)
-            KeywordType.IMAGE -> imageEmbedDao.findByVisualEmbedding(query)
-        }
-
-        return imageEmbeddingRelations
-            .parMap(context = dispatcher) { it.imageEntity.toExtraction() }
-    }
 
     private fun List<Extraction>.filterByDateRange(range: DateRange?) = when {
         range != null -> this.filter { it.dateAdded isIn range }
