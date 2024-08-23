c7d60185570f4090caaedb02fe0a5df7e7c3f490
nikolaDrljaca <devnikoladr@gmail.com>
2024-08-23 22:35:25 +0200

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.drbrosdev.extractor"
         minSdk = 24
         targetSdk = 34
-        versionCode = 15
-        versionName = "24.07"
+        versionCode = 16
+        versionName = "24.08"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
