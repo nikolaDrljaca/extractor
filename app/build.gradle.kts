aa2cc47fffd5b15ca55c2815d3b0aad63da730f9
nikolaDrljaca <devnikoladr@gmail.com>
2025-03-09 18:54:12 +0100

    wip: search screen redesign

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "com.drbrosdev.extractor"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19
+        versionCode = 20
         versionName = "25.03"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
