a39f2d143cb339b0ed8dc51e7d4102ed6dc6faaa
nikolaDrljaca <devnikoladr@gmail.com>
2025-03-25 19:54:29 +0100

    feature: showcase while indexing

--- app/src/main/java/com/drbrosdev/extractor/ui/components/showcase/ShowcaseComponent.kt
+++ app/src/main/java/com/drbrosdev/extractor/ui/components/showcase/ShowcaseComponent.kt
@@ -5,21 +5,26 @@ import com.drbrosdev.extractor.domain.model.ExtractionData
 import com.drbrosdev.extractor.domain.model.ExtractionStatus
 import com.drbrosdev.extractor.domain.usecase.extractor.TrackExtractionProgress
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.ticker
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.sample
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.flow.stateIn
-import kotlin.time.Duration.Companion.milliseconds
 
 class ShowcaseComponent(
     private val coroutineScope: CoroutineScope,
     private val trackExtractionProgress: TrackExtractionProgress,
     private val getMostRecentExtraction: suspend () -> ExtractionData?
 ) {
-    // NOTE: Try a tickerFlow with combine -> progressFlow.combine(ticker) {...}
+    private val tickerFlow = ticker(
+        delayMillis = ExtractorShowcaseDefaults.SHOWCASE_SAMPLE_RATE.toLong(),
+        initialDelayMillis = 0L
+    )
+        .consumeAsFlow()
+
     val state = trackExtractionProgress.invoke()
-        .map {
-            when(it) {
+        .combine(tickerFlow) { it, _ ->
+            when (it) {
                 is ExtractionStatus.Done -> ShowcaseState.Done
 
                 is ExtractionStatus.Running -> {
@@ -34,7 +39,6 @@ class ShowcaseComponent(
                 }
             }
         }
-        .sample(ExtractorShowcaseDefaults.SHOWCASE_SAMPLE_RATE.milliseconds)
         .stateIn(
             coroutineScope,
             // NOTE: maybe use WhileSubscribed here
